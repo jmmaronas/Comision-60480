a39ddc6e8008aae7518a0ca0c9ca958be2767d82
Juan Manuel Maroñas <jmmaronas@gmail.com>
2024-03-25 22:38:26 -0300

    clase-3

---
+++ src/ejemplo.js
@@ -0,0 +1,24 @@
+
+// let sumar = function(){
+//     return 5 + 2
+// }
+
+// let resultado = sumar()
+// //console.log(sumar())
+
+// console.log(resultado)
+
+// function sumar(param1, param2){
+//     return param1+param2;
+// }
+// const sumar = (num1, num2) => num1 + num2
+
+
+
+// let resultado = sumar(10, 5)
+
+// console.log(resultado);
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 // Actividad 01
 
 // const IVA = 1.21;
-// let importe = prompt("Ingresa el importe sobre el cual quieres calcular el IVA:");
+// let importe = parseFloat(prompt("Ingresa el importe sobre el cual quieres calcular el IVA:"))
 
-// const precioConIVA = imp, iva => imp * iva;
+// let resultado = precioConIVA(importe, IVA)
 
-// precioConIVA(importe, IVA)
+// function precioConIVA(valor, valorIva) {
+//     if(isNaN(valor)){
+//         return
+//     }else{
+//         return valor * valorIva
+//     }
+// }
+
+// console.log(precioConIVA(500, IVA))
+
+// console.log(resultado)
 
 //Actividad 02
 
-// const valorM2 = 31.83; //valor fijo del seguro por Metro 2
-// const comision = 1.025; //comisión del 2.5%
-// const M2 = prompt("Ingresa los Metros cuadrados de la propiedad a cotizar un seguro:");
-// const calcularPoliza = m2 => { M2 * valorM2 * comision };
-// const valorPoliza = calcularPoliza(120);
-// console.log("El costo de la póliza es: $", valorPoliza);
+let VALOR_M2 = 31.83; //valor fijo del seguro por Metro 2
+const COMISION = 1.025; //comisión del 2.5%
+
+function calcularPoliza(cantMetros, valorMetro, comision) {
+    return cantMetros * valorMetro * comision;
+}
+
+const M2 = prompt("Ingresa los Metros cuadrados de la propiedad a cotizar un seguro:");
+const valorPoliza = calcularPoliza(M2, VALOR_M2, COMISION);
+console.log("El costo de la póliza es: $", valorPoliza);
+const valorComsion = calcularPoliza(10, 1000, 1.15);
+const valorPolizaSegunUsuario = calcularPoliza(M2, prompt("valor del metro"), COMISION);
